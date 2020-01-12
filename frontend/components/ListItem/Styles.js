2924b527553e96776a78a929d49ecefb0a24df79
hurjun1995 <hurjun1995@gmail.com>
2020-01-12 00:36:29 -0800

    dictionary screen done with mock data

---
+++ frontend/components/ListItem/Styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "500"
+  },
+  body: {
+    fontSize: 14
+  }
+})
---
+++ frontend/components/ListItemDetail/Styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10
+  },
+  body: {
+    fontSize: 14
+  }
+})
