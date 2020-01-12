61ba3f716e74062b4451b66630a28f5e0b9bdd2c
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-01-11 18:49:10 -0800

    Add Chemical Count to Results Page

--- frontend/screens/ResultsScreen/ResultsScreen.js
+++ frontend/screens/ResultsScreen/ResultsScreen.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import {
-  // ScrollView,
-  Text,
-  View,
-} from 'react-native';
+import { Text, View } from 'react-native';
 import styles from './Styles';
 import ScoreDisplay from '../../components/ScoreDisplay/ScoreDisplay';
 
 export default function ResultsScreen() {
     let score = 56; // sample number
     let sampleText = getText();
+    let chemCount = 5;
     return (
         <View>
             <View style={styles.resultsHeader}>
                 <Text style={styles.resultsHeaderTitle}>Product Analysis</Text>
                 <ScoreDisplay />
             </View>
-        <Text style={styles.testText}>{ sampleText }</Text>
-    </View>
-  );
+            <Text style={styles.explanation}>This product contains
+                <Text style={styles.explanationNumber}> { chemCount } </Text>
+                ingredients from our list of harmful chemicals.</Text>
+            <Text style={styles.testText}>Chemicals</Text>
+        </View>
+    );
 }
 
 // sample function
 function getText() {
-  return "Results Screen";
+    return "Results Screen";
 }
 
 ResultsScreen.navigationOptions = {
     title: 'Results',
-  };
+};
 
--- frontend/screens/ResultsScreen/Styles.js
+++ frontend/screens/ResultsScreen/Styles.js
@@ -1,23 +1,31 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  testText: {
-    color: '#0f03fc',
-    fontSize: 20,
-  },
 
-  resultsHeader: {
+    resultsHeader: {
         flexDirection: 'row',
         padding: 28,
         alignItems: 'center',
         justifyContent: 'space-between',
-  },
+    },
 
-  resultsHeaderTitle: {
-      color: '#000000',
-      fontSize: 26,
-      fontWeight: 'bold',
-      letterSpacing: 1,
-  }
+    resultsHeaderTitle: {
+        color: '#000000',
+        fontSize: 26,
+        fontWeight: 'bold',
+        letterSpacing: 1,
+    },
   
+    explanation: {
+        color: '#000000',
+        fontSize: 18,
+        paddingLeft: 16,
+        paddingRight: 16,
+    },
+
+    explanationNumber: {
+        color: '#ff3b3b',
+        fontSize: 24,
+        fontWeight: 'bold'
+    }
 });
